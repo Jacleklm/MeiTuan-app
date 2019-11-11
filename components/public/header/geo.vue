65139ded1e36ea1511f51fa7a1568be362be1af3
Jacleklm <1061161721@qq.com>
2019-11-11 20:22:31 +0800

    'header-topbar-finish'

---
+++ components/public/header/geo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"></i>北京市
+    <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>[ 天津 香河 廊坊 ]
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
---
+++ components/public/header/searchbar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="search-panel">
+    <el-row class="m-header-searchbar">
+      <el-col
+        :span="3"
+        class="left"
+      >
+       <img
+        src="//s0.meituan.net/bs/fe-web-meituan/fa5f0f0/img/logo.png"
+        alt="美团"
+        >
+      </el-col>
+      <el-col
+        :span="15"
+        class="center">
+        <div class="wrapper">
+          <el-input
+            placeholder="搜索商家或地点"
+          >
+            1
+          </el-input>
+          <button class="el-button el-button--primary"><i class="el-icon-search"/></button>
+          <!-- 这里是focus后搜索框出现的部分和输入后出现的推荐部分 -->
+          <dl
+            style="display:none"
+            class="hotPlace">
+            <dt>热门搜索</dt>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+          <dl
+            style="display:none"
+            class="searchList">
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+        </div>
+        <!-- 这里是搜索框正下方的推荐部分 -->
+        <p class="suggest">
+          <a href="#">故宫</a>
+          <a href="#">故宫</a>
+          <a href="#">故宫</a>
+          <a href="#">故宫</a>
+        </p>
+        <ul class="nav">
+          <li><nuxt-link
+            to="/"
+            class="takeout">美团外卖</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="movie">猫眼电影</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="hotel">美团酒店</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="apartment">民宿/公寓</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="business">商家入驻</nuxt-link></li>
+        </ul>
+      </el-col>
+      <el-col
+        :span="6"
+        class="right">
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+</style>
